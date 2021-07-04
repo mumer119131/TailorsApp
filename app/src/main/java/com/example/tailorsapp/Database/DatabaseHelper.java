a5a8a1f67a09e2f2c77e7567f812bb9e2979974d
Muhammad <mumer119131@gmail.com>
2021-07-04 18:28:38 +0500

    SQL LITE DatabaseHelper ADDED...

---
+++ app/src/main/java/com/example/tailorsapp/Database/DatabaseHelper.java
@@ -0,0 +1,100 @@
+package com.example.tailorsapp.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="sqlDatabase.db";
+
+    public static  final String TABLE_NAME="Clients";
+    public static  final String ID="ID";
+    public static  final String NAME="NAME";
+    public static  final String PHONE="PHONE";
+    public static  final String LEG="LEG";
+    public static  final String ARM="ARM";
+    public static  final String CHEST="CHEST";
+    public static  final String NECK="NECK";
+    public static  final String FRONT_SIDE="FRONT";
+    public static  final String BACK_SIDE="BACK";
+    public static  final String DATE="DATE";
+    public static  final String ADDITIONAL_DETAILS="DETAILS";
+
+
+
+    private Context context;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context,DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TABLE_NAME+"("
+                +ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +NAME+" TEXT,"
+                 +PHONE+" TEXT,"
+                 +LEG+" INTEGER,"
+                 +ARM+" INTEGER,"
+                 +CHEST+" INTEGER,"
+                 +NECK+" INTEGER,"
+                 +FRONT_SIDE+" INTEGER,"
+                 +BACK_SIDE+" INTEGER,"
+                 +DATE+" DATE,"
+                 +ADDITIONAL_DETAILS+" TEXT"
+                 +");"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insert_in_clients(String name,String phone,String leg, String arm,String chest,String neck,String front,String back,String date,String details ){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(NAME,name);
+        values.put(PHONE,phone);
+        values.put(LEG,leg);
+        values.put(ARM,arm);
+        values.put(CHEST,chest);
+        values.put(NECK,neck);
+        values.put(FRONT_SIDE,front);
+        values.put(BACK_SIDE,back);
+        values.put(DATE,date);
+        values.put(ADDITIONAL_DETAILS,details);
+        long result= db.insert(TABLE_NAME,null,values);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result=db.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+        return result;
+    }
+    public Cursor getDatabyID(int id){
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor result=db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE "+ID+"="+id,null);
+        return result;
+
+    }
+    public boolean DeleteDataByID(int cli_id){
+        String id = Integer.toString(cli_id);
+        SQLiteDatabase db=this.getWritableDatabase();
+        long result=db.delete(TABLE_NAME,ID+"=?",new String[]{id});
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+}
--- app/src/main/java/com/example/tailorsapp/SplashScreen.java
+++ app/src/main/java/com/example/tailorsapp/SplashScreen.java
@@ -3,18 +3,30 @@ package com.example.tailorsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.tailorsapp.Database.DatabaseHelper;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private DatabaseHelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         mAuth=FirebaseAuth.getInstance();
+        myDB=new DatabaseHelper(this);
+        SQLiteDatabase db=myDB.getWritableDatabase();
+        boolean tableExist=DoesTableExists(db,"Clients");
+        if(!tableExist){
+            myDB.onCreate(db);
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -30,4 +42,15 @@ public class SplashScreen extends AppCompatActivity {
             }
         },3000);
     }
+
+    public boolean DoesTableExists( SQLiteDatabase db,String tableName){
+        try {
+            db.rawQuery("SELECT * FROM "+tableName,null);
+            return true;
+        }catch (SQLException e){
+            return false;
+        }
+
+    }
+
 }
